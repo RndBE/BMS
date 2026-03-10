7b7a6a1680e05a55c1180a7bc3a322e4cb670eae
RndBE <rnd.software.be@gmail.com>
2026-03-10 14:38:32 +0700

    Use Hash::make instead of bcrypt in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +18,7 @@ class DatabaseSeeder extends Seeder
             ['email' => 'superadmin@gmail.com'],
             [
                 'name'              => 'Super Admin',
-                'password'          => bcrypt('password'),
+                'password'          => Hash::make('password'),
                 'email_verified_at' => now(),
             ]
         );
