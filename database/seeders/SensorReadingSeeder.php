d776b2d4f4594d5757694793cf6f49021cb7fa29
RndBE <rnd.software.be@gmail.com>
2026-03-27 09:08:43 +0700

    feat: Add SensorReadingSeeder to generate sensor readings with configurable intervals and alert zone distribution, including backfilling the latest readings table.

--- database/seeders/SensorReadingSeeder.php
+++ database/seeders/SensorReadingSeeder.php
@@ -67,7 +67,7 @@ class SensorReadingSeeder extends Seeder
     public function run(): void
     {
         $from = Carbon::create(2026, 2, 1,  0,  0, 0);
-        $to   = Carbon::create(2026, 3, 26, 12, 59, 59);
+        $to   = Carbon::create(2026, 3, 27, 14, 00, 00);
 
         $totalMinutes = (int) $from->diffInMinutes($to);
         $totalPoints  = (int) ($totalMinutes / $this->intervalMinutes);
